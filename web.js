10327045579ca264f7b62430cbc253536df38e0c
Joe Dominic <thisisjoed@gmail.com>
2013-07-11 02:47:49 +0530

    Made read form index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs=require('fs');
 
+var buf=fs.readFileSync("index.html");
+var txt=buf.toString();
 var app = express.createServer(express.logger());
 
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(txt);
 });
 
 var port = process.env.PORT || 5000;
